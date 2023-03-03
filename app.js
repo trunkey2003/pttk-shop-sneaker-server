51c1a9b6f34bc8645bbfc3045cf057de8799589e
Cuong Phan <21520673@gm.uit.edu.vn>
2023-03-03 10:14:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
+
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./src/integration");
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
-// set port, listen for requests
+// set port
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
