47002d32bc7ab4fd5e7d38a92824e246e2aa1973
speeDy167 <trongphucphan7@gmail.com>
2023-03-04 19:31:15 +0700

    run with no error, ready to merge

---
+++ src/api/controllers/cart.controller.js
@@ -0,0 +1,72 @@
+const Cart = require("../../integration/cart");
+
+class CartController {
+  async addToCart(req, res) {
+    const {
+      productName,
+      quantity,
+      productImage,
+      productPrice,
+      userId,
+      productId,
+    } = req.body;
+    const cart = await Cart.create({
+      productName,
+      quantity,
+      productImage,
+      productPrice,
+      userId,
+      productId,
+    });
+
+    res.status(200).json({
+      success: true,
+      cart,
+    });
+  }
+
+  async updateCart(req, res) {
+    try {
+      const { quantity } = req.body;
+      const cart = await Cart.findByIdAndUpdate(req.params.id);
+
+      if (!cart) {
+        return next(new Error());
+      }
+
+      await cart.update({
+        quantity,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCartData(req, res) {
+    try {
+      const cartData = await Cart.find({ userId: req.user.id });
+      res.status(200).json({
+        success: true,
+        cartData,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async removeCartData(req, res) {
+    try {
+      const cartData = await Cart.findById({ userId: req.params.id });
+      if (!cartData) return next(new Error());
+      else await cartData.remove();
+      res.status(200).json({
+        success: true,
+        message: "Item removed from cart",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = new CartController();
--- src/api/routes/order.routes.js
+++ src/api/routes/order.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const order_router = express.Router();
+const OrderController = require("../controllers/order.controller");
 
+module.exports = (order_router) => {
+  order_router.get("/:id", OrderController.getOrder);
+  order_router.post("/create", OrderController.createOrder);
+  order_router.delete("/delete/:id", OrderController.deleteOrder);
+};
