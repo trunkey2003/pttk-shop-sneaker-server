a4213b28a3a1d7470b02ecb53353f192685713e7
Phan Pham Trong Phuc_UIT <100747299+speeDy167@users.noreply.github.com>
2023-03-05 12:30:26 +0700

    fix order routes

--- src/api/routes/order.routes.js
+++ src/api/routes/order.routes.js
@@ -2,6 +2,6 @@ const OrderController = require("../controllers/order.controller");
 
 module.exports = (order_router) => {
   order_router.get("/:id", OrderController.getOrder);
-  order_router.post("/create", OrderController.createOrder);
-  order_router.delete("/delete/:id", OrderController.deleteOrder);
+  order_router.post("/createOrder", OrderController.createOrder);
+  order_router.delete("/deleteOrder/:id", OrderController.deleteOrder);
 };
