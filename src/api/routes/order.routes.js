2eee57236158d590894643799f3557a456e712f2
trunkey2003 <21521049@gm.uit.edu.vn>
2023-05-20 18:34:13 +0700

    [ready to deploy] update endpoint

--- src/api/routes/order.routes.js
+++ src/api/routes/order.routes.js
@@ -1,7 +1,7 @@
 const OrderController = require("../controllers/order.controller");
 
 module.exports = (order_router) => {
-  order_router.get("/:id", OrderController.getOrder);
+  order_router.get("/api/:id", OrderController.getOrder);
   order_router.post("/createOrder", OrderController.createOrder);
   order_router.delete("/deleteOrder/:id", OrderController.deleteOrder);
 };
